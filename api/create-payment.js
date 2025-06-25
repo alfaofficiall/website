eace7ba03fb4e0ff0138c3b8d7a428798e32fba7
alfaofficiall <freefiremax18278@gmail.com>
2025-06-25 14:26:33 +0700

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -6,12 +6,10 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  // Jika metodenya OPTIONS, kirim respons OK (untuk preflight request)
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
 
-  // Kredensial rahasia Anda, disimpan di server
   const apikey = "alfa2025";
   const qrisCode = "00020101021126670016COM.NOBUBANK.WWW01189360050300000879140214140263240266770303UMI51440014ID.CO.QRIS.WWW0215ID20253948029410303UMI5204481253033605802ID5919ANDI CALL OK23853956005BLORA61055821162070703A0163044FEE";
   const { amount } = req.query;
