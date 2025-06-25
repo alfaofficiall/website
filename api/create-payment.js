d20763d20cdb4f2e63f2b127e90501b92bbe728e
alfaofficiall <freefiremax18278@gmail.com>
2025-06-25 14:51:39 +0700

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -1,15 +1,8 @@
 import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
-  // Atur header untuk mengizinkan permintaan dari mana saja (solusi CORS)
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
   const apikey = "alfa2025";
   const qrisCode = "00020101021126670016COM.NOBUBANK.WWW01189360050300000879140214140263240266770303UMI51440014ID.CO.QRIS.WWW0215ID20253948029410303UMI5204481253033605802ID5919ANDI CALL OK23853956005BLORA61055821162070703A0163044FEE";
   const { amount } = req.query;
@@ -25,7 +18,6 @@ export default async function handler(req, res) {
     const data = await apiResponse.json();
     res.status(200).json(data);
   } catch (error) {
-    console.error('Error di proxy create-payment:', error);
     res.status(500).json({ error: 'Gagal menghubungi server pembayaran.' });
   }
 }
